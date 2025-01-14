d486abdbe289459beb51fe3b4958668ae4f5b4e6
mariadvezza <maria.eugenia@grafeno.digital>
2025-01-14 18:44:02 -0300

    ajusta vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({

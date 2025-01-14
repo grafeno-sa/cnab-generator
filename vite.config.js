9fe25ac47fe4e13d2552ba2f5ffcca81cff117fe
mariadvezza <maria.eugenia@grafeno.digital>
2025-01-14 18:07:52 -0300

    teste vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/cnab-generator/',
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});

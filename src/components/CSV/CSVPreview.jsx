824408c567551c720876f850564c8d5f1797d38f
mariadvezza <maria.eugenia@grafeno.digital>
2025-01-15 18:14:05 -0300

    wip

---
+++ src/components/CSV/CSVPreview.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import ContentFormatter from "../../scripts/CSV/contentFormatter";
+
+import '../../styles/components/CNABPreview.css';
+
+const CNABPreview = ({ generatedLines }) => {
+  if (generatedLines.length >= 50000) {
+    return (
+      <textarea
+        value={'Preview desligado por questões de performance'}
+        className="cnab-preview"
+        readOnly/>
+    )
+  }
+  return (
+    <textarea
+      value={ContentFormatter().format(generatedLines)}
+      className="cnab-preview"
+      readOnly/>
+  )
+}
+
+CNABPreview.propTypes = {
+  generatedLines: PropTypes.array.isRequired,
+};
+
+export default CNABPreview
---
+++ src/scripts/CSV/contentFormatter.js
@@ -0,0 +1,23 @@
+import { CSV_LINE_FIELDS } from './lineFields';
+
+const ContentFormatter = () => {
+  const format = (generatedLines) => {
+    const header = formatHeader()
+    const data = formatData(generatedLines);
+    const content = [header, ...data]
+
+    return content.join('\n')
+  }
+
+  const formatHeader = () => {
+    return CSV_LINE_FIELDS.map(field => field.name).join(';')
+  }
+
+  const formatData = (generatedLines) => {
+    return generatedLines.map(line => line.data.join(';'))
+  }
+
+  return { format }
+}
+
+export default ContentFormatter;

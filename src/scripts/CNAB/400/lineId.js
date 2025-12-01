9192cc59eac9f1f660f7e789689ef0e5f2bf50fd
Mage Vezza <maria.eugenia@grafeno.digital>
2025-12-01 10:58:35 -0300

    refactoring constants

---
+++ src/scripts/CNAB/400/lineId.js
@@ -0,0 +1,6 @@
+export const TRAILER_ID = '9';
+export const HEADER_ID = '0';
+export const REGISTER_1_ID = '1';
+export const REGISTER_2_ID = '2';
+export const REGISTER_3_ID = '3';
+export const REGISTER_7_ID = '7';
